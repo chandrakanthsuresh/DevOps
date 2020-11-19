d9eb46c25be5754f93217ba372e8a6382463ada8
chandrakanthsuresh <67475120+chandrakanthsuresh@users.noreply.github.com>
2020-11-19 12:02:59 -0500

    Update return_first_recurring_char.ps1

--- return_first_recurring_char.ps1
+++ return_first_recurring_char.ps1
@@ -2,8 +2,17 @@
 param(
 [string] $foo)
 try{
-
-
+$charar = $foo.ToCharArray()
+$tempchar = [char[]]:new($charar.count)
+$charar | ForEach{
+if($tempchar.contains($_)){
+Write-Host "First recurring charecter is $_"
+break
+}
+else{
+$tempchar += $_
+}
+}
 exit 0
 }
 catch{
