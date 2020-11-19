8f5c017f1b4ee43bc1e67c808469deb602e3a523
chandrakanthsuresh <67475120+chandrakanthsuresh@users.noreply.github.com>
2020-11-19 12:47:41 -0500

    Update Executable_return_first_recurring_char.ps1

--- Executable_return_first_recurring_char.ps1
+++ Executable_return_first_recurring_char.ps1
@@ -1,1 +1,27 @@
+funtion get-firstrecurringcharacter {
+[CmdletBinding()]
+param(
+[string] $foo
+)
+try{
+$charar = $foo.ToCharArray()
+$tempchar = [char[]]::new($charar.count)
+$charar | ForEach{
+if($tempchar.contains($_)){
+Write-Host "FIrst recurring character is $_"
+break
+}
+else{
+$tempchar += $_
+}
+}
+Write-Host "No Recurring character found"
+exit 0
+}
+catch{
+Write-Host "Exception Occured $_.Exception.Message"
+Exit 1
+}
+}
 
+get-firstrecurringcharacter -foo 'abcdedcba'
